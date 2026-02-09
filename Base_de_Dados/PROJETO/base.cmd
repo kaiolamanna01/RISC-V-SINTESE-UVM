854047063fede74d7effaeab44b3d60f643242da
Kaio <kaio.lamanna5@gmail.com>
2026-02-08 22:59:15 -0300

    oi

--- Base_de_Dados/PROJETO/base.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 22.17-s071_1, built Sep 12 2024 12:37:05
-
-# Date: Mon Jan 19 23:15:34 2026
-# Host: microeletronica-043 (x86_64 w/Linux 4.18.0-553.81.1.el8_10.x86_64) (6cores*12cpus*1physical cpu*Intel(R) Core(TM) i7-8700 CPU @ 3.20GHz 12288KB)
-# OS:   Rocky Linux release 8.10 (Green Obsidian)
-
-source genus_script.tcl
